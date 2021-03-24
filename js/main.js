f54b6c2e8325acc71a8b03a235e78f63e7b93ec8
joycesb <joycesouzabarboza@gmail.com>
2021-03-24 13:19:56 -0400

    ajax do form

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// $("form").submit()
+
+function enviar(){
+    debugger
+    var nome = $('#inputName').val();
+    var sobrenome= $('#inputSobrenome').val();
+    var email= $('#inputEmail4').val();
+    var dataNascimento= $('#inputData').val();
+    var estadoCivil= $('#inputEstadoCivil').val();
+    var conjuge= $('#inputConjuge').val();
+    var nomeMae= $('#inputNomeMae').val();
+    var nomePai= $('#inputNomePai').val();
+    var escolaridade= $('#inputEscolaridade').val();
+    var profisssao= $('#inputProfissao').val();
+    var tel= $('#txtTelefone').val();
+    var cel= $('#txtCel').val();
+    var bairro= $('#inputBairro').val();
+    var complemento= $('#inputAddress2').val();
+    var cidade= $('#inputCity').val();
+    var estado= $('#inputUF').val();
+    var batizado= $("[name='gridRadios']").val();
+    var dataBatismo= $('#inputDataBatismo').val();
+    var situacao= $('#txtCel').val();
+
+    $.ajax({
+        url : "https://armc-api-dev.heltec.com.br/api/auth/teste",
+        method : "POST",
+        contentType : 'application/json',
+        dataType : 'json',
+        data :
+            `{ "nome":"${nome}"},
+            { "sobrenome":"${sobrenome}"},
+            { "email":"${email}"},
+            { "dataNascimento":"${dataNascimento}"},
+            { "estadoCivil":"${estadoCivil}"},
+            { "conjuge":"${conjuge}"},
+            { "nomeMae":"${nomeMae}"},
+            { "nomePai":"${nomePai}"},
+            { "escolaridade":"${escolaridade}"},
+            { "profisssao":"${profisssao}"},
+            { "telefone":"${tel}"},
+            { "celular":"${cel}"},
+            { "bairro":"${bairro}"},
+            { "complemento":"${complemento}"},
+            { "estado":"${estado}"},
+            { "batizado":"${batizado}"},
+            { "dataBatismo":"${dataBatismo}"},
+            { "status":"${situacao}"}`,
+        beforeSend : function(){
+             $("#resultado").html("ENVIANDO...");
+        }
+    })
+    .done(function(msg){
+        $("#resultado").html(msg);
+    })
+    .fail(function(jqXHR, textStatus, msg){
+        alert(msg);
+    });
+}
